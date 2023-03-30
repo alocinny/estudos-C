f3f3ec4894ae1f6882d5a9478399f1db4e1a1a6c
benny <124681388+alocinny@users.noreply.github.com>
2023-03-29 21:50:10 -0300

    caractere

---
+++ aulas/aula 2/aula 2.2/caractere.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char letra = 'h';
+    int numeroInt = 50;
+    float notaFinal = 8.9;
+
+    printf("Valor: %c %.2f %d", letra, notaFinal, numeroInt);
+    printf("\n");
+    system("pause");
+    return 0;
+}
