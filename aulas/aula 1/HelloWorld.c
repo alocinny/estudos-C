5b7919c9be94fa91fa3c99a309673b3f8b65c3cb
benny <124681388+alocinny@users.noreply.github.com>
2023-03-29 21:04:02 -0300

    básico

---
+++ aulas/aula 1/HelloWorld.c
@@ -0,0 +1,12 @@
+int main() 
+{
+    /*EXIBIR TEXTO NO CONSOLE*/
+    /*TEM QUE ESCREVER PRINTF EM MINÚSCULO*/
+    /*\N -> QUEBRA DE LINHA*/
+    /*PONTO E VIRGULA OBRIGATÓRIO POIS INDICA TÉRMINO DE LINHA*/
+    
+    printf("Hello, world!\n");
+
+    system("pause");
+    return 0;
+}
